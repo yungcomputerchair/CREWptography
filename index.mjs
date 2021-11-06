3e3c88f7d3c6e9f0a4b2a5416e6696028a489e49
Robbie <5222372+robbie01@users.noreply.github.com>
2021-11-06 16:54:25 -0400

    ababababababa

--- index.mjs
+++ index.mjs
@@ -9,5 +9,25 @@ window.document.getElementById('generateButton').addEventListener('click', async
     e.preventDefault()
 
     let amongus = await generate_keypair()
-    window.document.querySelector('#yourself textarea').textContent = amongus
+    window.document.querySelector('#yourself textarea').value = amongus
+})
+
+window.document.getElementById('confirmButton').addEventListener('click', async e => {
+    e.preventDefault()
+
+    await set_crewmatepub(window.document.querySelector('#crewmate textarea').value)
+})
+
+window.document.getElementById('sendButton').addEventListener('click', async e => {
+    e.preventDefault()
+
+    let plain = window.document.getElementById('sendinput').value
+    window.document.getElementById('sendoutput').value = await encrypt_comms(plain)
+})
+
+window.document.getElementById('receiveButton').addEventListener('click', async e => {
+    e.preventDefault()
+
+    let cipher = window.document.getElementById('recvinput').value
+    window.document.getElementById('recvoutput').value = await decrypt_comms(cipher)
 })
